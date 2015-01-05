acd89f3ca6dece2e2f657cc6a976c2d3e88ea49d
Patrick Malik <austria.p@hotmail.com>
2015-01-05 10:55:26 +0100

    first commit

---
+++ A05-Rueckwaertssalto/src/at/pm/rs/main/test.java
@@ -0,0 +1,11 @@
+package at.pm.rs.main;
+
+/**
+ * @author Stefan Polydor
+ * @version 05.01.15
+ */
+public class test {
+	public static void main(String[] args) {
+		System.out.println("Hallo :D");
+	}
+}
--- src/test.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author Stefan Polydor
- * @version 05.01.15
- */
-public class test {
-}
