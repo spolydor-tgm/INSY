fc0a10a24b76a2f4f73bfb554036ba787c6e477f
Patrick Malik <austria.p@hotmail.com>
2015-01-14 08:59:41 +0100

    Added graphics classes and modified the UML

---
+++ src/at/pm/rs/graphics/TextWriter.java
@@ -0,0 +1,23 @@
+package at.pm.rs.graphics;
+
+import at.pm.rs.connection.SetOfData;
+
+public class TextWriter implements Writer{
+
+	String outputDir;
+
+	@Override
+	public void print(SetOfData data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getOutputDir() {
+		return outputDir;
+	}
+
+	public void setOutputDir(String outputDir) {
+		this.outputDir = outputDir;
+	}
+	
+}
