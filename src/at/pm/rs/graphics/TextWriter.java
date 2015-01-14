6482fe44dec8594f28a3d17c9a5173c58f424bf9
Patrick Malik <austria.p@hotmail.com>
2015-01-14 09:28:28 +0100

    Added the Writers and commented

--- src/at/pm/rs/graphics/TextWriter.java
+++ src/at/pm/rs/graphics/TextWriter.java
@@ -1,23 +1,44 @@
 package at.pm.rs.graphics;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 import at.pm.rs.connection.SetOfData;
 
-public class TextWriter implements Writer{
+/**
+ * This class creates a Writer object that prints text to a file. It generates
+ * from the given SetOfData the fitting relations model.
+ * 
+ * @author Patrick Malik
+ * @version 20150114
+ */
+public class TextWriter extends FileWriter {
 
-	String outputDir;
-
+	/**
+	 * prints the given SetOfData as RM to a file.
+	 * 
+	 * @see at.pm.rs.graphics.Writer#print(at.pm.rs.connection.SetOfData)
+	 * @param data
+	 *            the SetOfData to be printed.
+	 */
 	@Override
 	public void print(SetOfData data) {
-		// TODO Auto-generated method stub
+		PrintWriter writer = null;
+		try {
+			writer = new PrintWriter("the-file-name.txt", "UTF-8");
+		} catch (FileNotFoundException e) {
+			//TODO logger error
+		} catch (UnsupportedEncodingException e) {
+			// TODO Logger error
+		}
+		writer.println("The first line");
+		writer.println("The second line");
+		writer.close();
+	}
+
+	private String model(SetOfData data) {
 		
+		return null;
 	}
-
-	public String getOutputDir() {
-		return outputDir;
-	}
-
-	public void setOutputDir(String outputDir) {
-		this.outputDir = outputDir;
-	}
-	
 }
