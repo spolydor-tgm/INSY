a7fdb4d91d67f0b82eb4b988e9c98f20da3edb4e
spolydor-tgm <spolydor@student.tgm.ac.at>
2015-01-17 18:43:47 +0100

    added feauture for the output if there are only some columns selected

--- src/connection/Connection.java
+++ src/connection/Connection.java
@@ -20,14 +20,15 @@ public class Connection {
 
 	private String selectStatementString = "Select ";
 
+	/**
+	 * Creating the SELECT Statement and the connection to a MySQL DBMS
+	 * @param arguments parsed Options from the cli.ArgumentParser
+	 * @throws SQLException if there is a problem by connecting to the DBMS or by executing the SELECT Statement
+	 */
 	public Connection(String[] arguments) throws SQLException {
-		this.connect(arguments[0], arguments[3], arguments[1], arguments[2]);
+		this.connect(arguments[0], arguments[3], arguments[1], arguments[2]); // Establishing the connection to the DBMS
 
-		/*
-		if (arguments[8].equals(".classpath"))
-			selectStatementString += "* from ";
-		else
-		*/
+		// Creating the SELECT Statement
 		selectStatementString += arguments[8] + " from ";
 
 		selectStatementString += arguments[10] + " ";
@@ -45,44 +46,73 @@ public class Connection {
 		trennzeichen = arguments[7]; // Saving the trennzeichen
 
 		Statement statement = conn.createStatement();
-		resultSet = statement.executeQuery(selectStatementString);
-		ResultSetMetaData rsmd = resultSet.getMetaData();
-		columns = rsmd.getColumnCount();
+		resultSet = statement.executeQuery(selectStatementString); // Executing the SELECT Statement
+		if (arguments[8].equals("*")) {
+			ResultSetMetaData rsmd = resultSet.getMetaData();
+			columns = rsmd.getColumnCount(); // Get ColumnCount for the Specified Table in the SELECT Statement
+		} else // If there are only some Columns which will be selected
+			columns = arguments[8].split(",").length;
 	}
 
+	/**
+	 *
+	 * @return ResultSet with the executed Query
+	 */
 	public ResultSet getResultSet() {
 		return resultSet;
 	}
 
+	/**
+	 *
+	 * @return Columns from the table or which are selected from the Table
+	 */
 	public int getColumns() {
 		return columns;
 	}
 
+	/**
+	 *
+	 * @return Trennzeichen to seperate the columns values in a row
+	 */
 	public String getTrennzeichen() {
 		return trennzeichen;
 	}
 
+	/**
+	 *
+	 * @return OutputType null if it will be printed to the console or a String which represents the filename
+	 */
 	public String getOutputType() {
 		if (outputType == null)
 			return null;
 		return outputType;
 	}
 
+	/**
+	 * Closing all open Connections
+	 */
 	public void	closeConnection() {
 		try {
-			resultSet.close();
-			conn.close();
-		} catch (SQLException e) {
+			resultSet.close(); // Closing the ResultSet
+			conn.close(); // Closing the Connectnion
+		} catch (SQLException e) { // If there is an error, shutdown the whole program
 			System.exit(0);
 		}
 	}
 
+	/**
+	 * Connect to a MySQL DBMS
+	 * @param hostname ip (URI) of the DBMS
+	 * @param DBname Database name
+	 * @param username a username of an user in the DBMS
+	 * @param pwd password for the user
+	 */
 	public void connect(String hostname, String DBname, String username, String pwd) {
 		try {
-			// Treiber laden
+			// Loading the Driver
 			Class.forName("com.mysql.jdbc.Driver");
 
-			// Verbindung mit dem DBMS herstellen
+			// Establish the connection to the DBMS
 			String url = "jdbc:mysql://" + hostname + "/" + DBname;
 			conn = DriverManager.getConnection(url, username, pwd);
 		} catch (ClassNotFoundException e) {
