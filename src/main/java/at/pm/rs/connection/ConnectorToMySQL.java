6553914a14ade3d843459b021541b7f5aa325d4a
spolydor-tgm <spolydor@student.tgm.ac.at>
2015-02-14 20:56:31 +0100

    edit main

--- src/main/java/at/pm/rs/connection/ConnectorToMySQL.java
+++ src/main/java/at/pm/rs/connection/ConnectorToMySQL.java
@@ -1,9 +1,5 @@
 package at.pm.rs.connection;
 
-import at.pm.rs.graphics.FileWriter;
-import at.pm.rs.graphics.TextWriter;
-import at.pm.rs.utils.ArgumentParser;
-
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -151,33 +147,5 @@ public class ConnectorToMySQL implements ConnectorTo{
 	public void closeConnections() throws SQLException {
 		rs.close();
 		conn.close();
-	}
-
-	public static void main(String[] args) {
-		ArgumentParser ap = new ArgumentParser();
-		//new ConnectorToMySQL(ap.parseArguments(args));
-		try {
-			ConnectorToMySQL connectorToMySQL = new ConnectorToMySQL(ap.parseArguments(args));
-			ArrayList<TableData> test = connectorToMySQL.readAllFromAllTables();
-			for (TableData data : test) {
-				System.out.print(data.getTableName());
-				for (SetOfData setOfData : data.getSetOfData())
-					System.out.print(setOfData.toString());
-
-				System.out.println("" + '\n');
-			}
-
-			FileWriter w = new TextWriter();
-			w.setOutputDir("src");
-			TableData[] te = new TableData[test.size()];
-			for (int x = 0; x < te.length; x++)
-				te[x] = test.get(x);
-			w.print(te);
-			connectorToMySQL.closeConnections();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException ee) {
-			ee.printStackTrace();
-		}
 	}
 }
