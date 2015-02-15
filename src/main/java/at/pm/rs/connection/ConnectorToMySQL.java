e5cd5839589699090dad1560ceccbd0a2cfb50e2
spolydor-tgm <spolydor@student.tgm.ac.at>
2015-02-15 18:39:40 +0100

    fixed problem for FK. written to wrong table

--- src/main/java/at/pm/rs/connection/ConnectorToMySQL.java
+++ src/main/java/at/pm/rs/connection/ConnectorToMySQL.java
@@ -85,10 +85,12 @@ public class ConnectorToMySQL implements ConnectorTo{
 				x = 0;
 				while (x < tableDatas.size()) {
 					TableData tableData = tableDatas.get(x);
+					System.out.println(tableData.getTableName());
 					if (tableData.getTableName().equals(rs.getString("FKTABLE_NAME"))) {
 						for (int y = 0; y < tableData.getSetOfData().size(); y++) {
 							if (tableData.getSetOfData().get(y).getName().equals(rs.getString("FKCOLUMN_NAME"))) {
-								tableData.getSetOfData().get(y).setFk(rs.getString("FKTABLE_NAME") + "." + rs.getString("FKCOLUMN_NAME"));
+								// tableData.getSetOfData().get(y).setFk(rs.getString("FKTABLE_NAME") + "." + rs.getString("FKCOLUMN_NAME"));
+								tableData.getSetOfData().get(y).setFk(tablenames.get(xx) + "." + rs.getString("FKCOLUMN_NAME"));
 								break;
 							}
 						}
